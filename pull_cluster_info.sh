c62392991cd402a4975a29fadd6264aef4ac3339
Jimmy Mills <jimmy@popdog.com>
2021-05-23 14:10:32 -0600

    Turn off debug in pull_cluster_info

--- pull_cluster_info.sh
+++ pull_cluster_info.sh
@@ -3,5 +3,5 @@
 mkdir -p $1
 kinds="deploy svc ing cm secret pvc job cronjob ds sts"
 for kind in $kinds; do
-  echo "kubectl --context=$2 get $kind -o yaml > ${kind}.yaml"
+  kubectl --context=$2 get $kind -o yaml > ${kind}.yaml
 done
