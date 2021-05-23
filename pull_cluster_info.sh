9978e63b81ae2eaa09d8c31d75c511f17c1ce3fd
Jimmy Mills <jimmy@popdog.com>
2021-05-23 14:11:55 -0600

    Fix target folder for cluster files

--- pull_cluster_info.sh
+++ pull_cluster_info.sh
@@ -3,5 +3,5 @@
 mkdir -p $1
 kinds="deploy svc ing cm secret pvc job cronjob ds sts"
 for kind in $kinds; do
-  kubectl --context=$2 get $kind -o yaml > ${kind}.yaml
+  kubectl --context=$2 get $kind -o yaml > "${1}/${kind}.yaml"
 done
